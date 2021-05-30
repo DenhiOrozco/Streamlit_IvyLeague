ad620fbf070455815b8b7ed3de8806e3014c82cb
DenhiOrozco <denhiparati@gmail.com>
2021-05-30 15:04:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -41,3 +41,28 @@ st.write(df.sample(5))
 st.text('Esta es la gráfica de distribución por score del GRE')
 distribution_GRE = pd.DataFrame(df['GRE Score'].value_counts())
 st.bar_chart(distribution_GRE)
+
+#Aquí comenzamos con el entrenamiento del modelo. 
+modelTraining= st.beta_container()
+with modelTraining:
+    st.header('Entrenamiento del modelo')
+    st.text('En esta sección entrenaremos al modelo')
+
+Y= df ['Chance of Admit']
+data_X= pd.DataFrame(df, columns=['GRE Score', 'TOEFL Score', 'University Rating', 'SOP',
+       'LOR ', 'CGPA', 'Research'])
+X= data_X
+
+#Aquí voy a generar x_train, x_test, y_train, y_test utilizando la función train_test_split y asignándole un test_size del 25%
+x_train, x_test, y_train, y_test = train_test_split (X, Y, test_size=0.25)
+
+#Aquí estoy almacenando los parámetro del modelo lineal en la variable lm. 
+lm=linear_model.LinearRegression()
+
+# guardo en la variable model el entrenamiento del modelo con las variables "_train"
+model=lm.fit(x_train, y_train)
+
+prediciones = lm.predict(x_test)
+
+#sns.regplot(y_test, prediciones)
+
